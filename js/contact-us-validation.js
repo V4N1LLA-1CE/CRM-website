731ea30394495613bd4081ce7227751f878ced60
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-08-20 17:03:03 +1000

    refactor: revert to using regex pattern in html

--- js/contact-us-validation.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById("contact-form");
-form.addEventListener("submit", () => {
-  let phoneNum = document.getElementById("phone").value;
-  if (isNaN(phoneNum) || phoneNum.length !== 10) {
-    alert("The phone number must be a number that is 10 length");
-    return false;
-  }
-});
