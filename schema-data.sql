8df21ed37c382ce526101bc314ef422f26248224
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-08-22 17:32:24 +1000

    feat: complete full db schema with png

--- schema-data.sql
+++ schema-data.sql
@@ -13,7 +13,8 @@ CREATE TABLE `Project` (
   `technique_required` varchar(255),
   `due_date` datetime,
   `pmt_link` varchar(255),
-  `org_id` int
+  `org_id` int,
+  `contractor_id` int
 );
 
 CREATE TABLE `Organisation` (
@@ -24,4 +25,29 @@ CREATE TABLE `Organisation` (
   `industry` varchar(255)
 );
 
+CREATE TABLE `Contractor` (
+  `id` integer PRIMARY KEY,
+  `first_name` varchar(255),
+  `last_name` varchar(255),
+  `specialisation` varchar(255),
+  `email` varchar(255),
+  `phone_number` varchar(255),
+  `address` varchar(255)
+);
+
+CREATE TABLE `Contact_Us` (
+  `id` integer PRIMARY KEY,
+  `first_name` varchar(255),
+  `last_name` varchar(255),
+  `phone_number` varchar(255),
+  `message` varchar(255),
+  `replied` boolean,
+  `org_id` int
+);
+
+ALTER TABLE `Contact_Us` ADD FOREIGN KEY (`org_id`) REFERENCES `Organisation` (`id`);
+
 ALTER TABLE `Project` ADD FOREIGN KEY (`org_id`) REFERENCES `Organisation` (`id`);
+
+ALTER TABLE `Project` ADD FOREIGN KEY (`contractor_id`) REFERENCES `Contractor` (`id`);
+
