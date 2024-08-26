cb6c0c4562d8bf4fd661d4ba62dd4201699008fb
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-08-26 14:12:46 +1000

    feat: add navbar and background to dashboard page

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,39 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Dashboard</title>
-  </head>
-  <body>
-    <h1>Dashboard</h1>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Dashboard</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body style="background-image: url('./assets/img/white-bg.jpg');">
+  <header>
+    <nav class="navbar bg-dark border-bottom border-body navbar-expand-lg" data-bs-theme="dark">
+      <div class="container-fluid gap-3">
+        <a href="/">
+          <img src="./assets/img/b2b-logo.svg" width="220" alt="Buiness logo" />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" href="#">Dashboard</a>
+            <a class="nav-link" href="./projects.php">Projects</a>
+            <a class="nav-link" href="./contractors.php">Contractors</a>
+            <a class="nav-link" href="./organisations.php">Organisations</a>
+            <a class="nav-link " href="./users.php">Users</a>
+            <a class="nav-link" href="./contact.php">Contact Us</a>
+          </div>
+          <a href="login.php"><button class="btn btn-danger">Logout</button></a>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
 </html>
