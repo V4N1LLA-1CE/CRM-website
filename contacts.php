ee3c1dfca7f7e05f5800e2a3bd9b1bf7abd19df2
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-08-28 06:08:42 +1000

    fix: contact messages page page is now under a protected route using sessions

--- contacts.php
+++ contacts.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+// check if user is logged in
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+  header("Location: login.php");
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
