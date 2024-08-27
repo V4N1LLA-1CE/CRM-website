c5403cfd254628b0b813e9c3acfdb68e52866133
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-08-27 15:17:39 +1000

    fix: navbar is now mobile responsive

--- dashboard.php
+++ dashboard.php
@@ -18,24 +18,34 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 
 <body style="background-image: url('./assets/img/white-bg.jpg');">
   <header>
-    <nav class="navbar bg-dark border-bottom border-body navbar-expand-lg" data-bs-theme="dark">
-      <div class="container-fluid gap-3">
-        <a href="/">
-          <img src="./assets/img/b2b-logo.svg" width="220" alt="Buiness logo" />
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg" style="background-color: #e6f0f1;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/"><img src="./assets/img/b2b-logo.svg" alt="b2b-logo" width="220" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active" href="dashboard.php">Dashboard</a>
-            <a class="nav-link" href="projects.php">Projects</a>
-            <a class="nav-link" href="contractors.php">Contractors</a>
-            <a class="nav-link" href="organisations.php">Organisations</a>
-            <a class="nav-link " href="users.php">Users</a>
-            <a class="nav-link" href="contact.php">Contact Us</a>
-          </div>
-          <a href="logout.php"><button class="btn btn-danger">Logout</button></a>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+          <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="./dashboard.php">Dashboard</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./projects.php">Projects</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link " href="./contractors.php">Contractors</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./organisations.php">Organisations</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./users.php">Users</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./contacts.php">Contacts</a>
+            </li>
+          </ul>
+          <a href="./logout.php" class="nav-link"><button class="btn btn-danger">Logout</button></a>
         </div>
       </div>
     </nav>
